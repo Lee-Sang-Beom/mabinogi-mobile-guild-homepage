f0ecd0235d163715d876b64b4ee8919bda7a5441
Lee-Sang-Beom <tkdqja975@naver.com>
2025-04-14 13:39:29 +0900

    필요 패키지 설치

---
+++ shared/constants/constants.ts
@@ -0,0 +1,4 @@
+export const MESSAGE_NETWORK_STATUS_ERROR = '통신 과정에서 오류가 발생했습니다. 잠시 후 다시 시도해 주세요'
+export const MESSAGE_UNKNOWN_ERROR = '알 수 없는 오류가 발생했습니다. 잠시 후 다시 시도해 주세요'
+export const MESSAGE_NOTFOUND_ERROR = '페이지를 찾을 수 없습니다.'
+export const MESSAGE_TIMEOUT_ERROR = '네트워크 요청 시간이 초과되었습니다. 잠시 후 다시 시도해주세요.'
