b63f1e5582477c412a7dc3d844a0d430eb2c0be8
Lee-Sang-Beom <tkdqja975@naver.com>
2025-05-02 16:24:05 +0900

    chore: 로딩컴포넌트 수정 및 공지사항영역 리다이엑트코드 추가

--- app/(auth)/announcements/page.tsx
+++ app/(auth)/announcements/page.tsx
@@ -1,5 +1,12 @@
 import AnnouncementsList from "./_component/announcement-list";
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/app/api/auth/[...nextauth]/AuthOptions'
+import { redirect } from 'next/navigation'
 
-export default function Page() {
+export default async function Page() {
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user) {
+    redirect('/login');
+  }
   return <AnnouncementsList />
 }
