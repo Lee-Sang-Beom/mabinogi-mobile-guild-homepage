47b5cdd1242984e52f982b30cb8c0a8648b07fc5
Lee-Sang-Beom <tkdqja975@naver.com>
2025-05-30 15:02:00 +0900

    chore: 음악 파일 추가

--- app/(auth)/game/util.ts
+++ app/(auth)/game/util.ts
@@ -2,6 +2,9 @@ const audioList: string[] = [
   "/audios/fergus-song.mp3",
   "/audios/with-glory-i-shall-fall.mp3",
   "/audios/against-the-tide.mp3",
+  "/audios/escaping-the-sandworm.mp3",
+  "/audios/light-of-nibel.mp3",
+  "/audios/test-drive.mp3",
 ];
 
 // 랜덤 오디오 선택 함수
